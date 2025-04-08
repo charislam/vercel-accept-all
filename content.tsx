1ced335e347881660de4b07e5058c0287d6420c8
Charis Lam <26616127+charislam@users.noreply.github.com>
2025-04-08 12:38:03 -0400

    fix selectors

--- content.tsx
+++ content.tsx
@@ -1,25 +1,15 @@
 import { useMessage } from "@plasmohq/messaging/hook"
 
 const openVercelLinks = () => {
-  const vercelNames = [
-    "Vercel – docs",
-    "Vercel – studio",
-    "Vercel – studio-self-hosted",
-    "Vercel – studio-staging",
-    "Vercel – zone-www-dot-com",
-    "Vercel – database-new",
-    "Vercel - design-system"
-  ]
   const vercelItems = Array.from(
-    document.body.querySelectorAll(".merge-status-list .merge-status-item")
-  ).filter((item) =>
-    vercelNames.some((name) => item.textContent.includes(name))
+    document.body.querySelectorAll(
+      "[aria-label='Checks'] [aria-label='failing checks'] li[aria-label^='Vercel']"
+    )
   )
   const hrefs = vercelItems.map((item) =>
-    item.querySelector("a.status-actions")?.getAttribute("href")
+    item.querySelector("a[href^='https://vercel.com']")?.getAttribute("href")
   )
 
-  console.log("hrefs: ", hrefs)
   hrefs.forEach((href) => {
     if (href) {
       window.open(href, "_blank")
